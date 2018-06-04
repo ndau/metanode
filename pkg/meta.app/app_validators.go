e7394d0082a1a9491823e500441e6b405e30db19
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-06-04 17:26:18 +0200

    Add UpdateValidator method

---
+++ pkg/meta.app/app_validators.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"github.com/tendermint/abci/types"
+)
+
+// UpdateValidator updates the app's internal state with the given validator
+func (app *App) UpdateValidator(v types.Validator) {
+	logger := app.logger.With("method", "updateValidator")
+	logger.Info("entered method", "Power", v.GetPower(), "PubKey", v.GetPubKey())
+	app.state.UpdateValidator(app.db, v)
+
+	// we only update the changes array after updating the tree
+	app.ValUpdates = append(app.ValUpdates, v)
+	logger.Info("exiting OK", "app.ValUpdates", app.ValUpdates)
+}
