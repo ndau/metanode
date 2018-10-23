4f60b392a6b55e89ea2dbf0bffd44d97ef201b64
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2018-10-23 13:01:47 +0200

    add test helpers for validators

---
+++ pkg/meta/state/validator_helpers.go
@@ -0,0 +1,46 @@
+package state
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmconv "github.com/tendermint/tendermint/types"
+)
+
+// ValUpdateToVal is a test helper which converts between these equivalent types
+func ValUpdateToVal(t *testing.T, vu abci.ValidatorUpdate) abci.Validator {
+	cpk, err := tmconv.PB2TM.PubKey(vu.PubKey)
+	require.NoError(t, err)
+	return abci.Validator{
+		Address: cpk.Address(),
+		Power:   vu.Power,
+	}
+}
+
+// ValUpdatesToVals is a test helper which converts slices between these equivalent types
+func ValUpdatesToVals(t *testing.T, vus []abci.ValidatorUpdate) []abci.Validator {
+	out := make([]abci.Validator, len(vus))
+	for i := 0; i < len(vus); i++ {
+		out[i] = ValUpdateToVal(t, vus[i])
+	}
+	return out
+}
+
+// ValidatorsAreEquivalent is a test helper which ensures that two lists of
+// validators are in fact equivalent.
+func ValidatorsAreEquivalent(t *testing.T, a, b []abci.Validator) {
+	require.Equal(t, len(a), len(b))
+	// convert a and b into maps of public key to power
+	// this discards irrelevant tendermint-specific internal data
+	// which we don't care about
+	aMap := make(map[string]int64)
+	bMap := make(map[string]int64)
+	for i := 0; i < len(a); i++ {
+		aMap[string(a[i].Address)] = a[i].Power
+		bMap[string(b[i].Address)] = b[i].Power
+	}
+	require.Equal(t, len(a), len(aMap), "a list had duplicate public keys")
+	require.Equal(t, len(b), len(bMap), "b list had duplicate public keys")
+	require.Equal(t, aMap, bMap)
+}
