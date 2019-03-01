e859cfdecfa30ad21ca2a6d81b9640634a6df7ff
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-03-01 18:17:42 +0100

    WIP: Add history field to metastate
    Tear out lots of existing metastate marshaling code; replace with
    generated code. Change metastate types to ones easier to work with
    in go code.
    
    ./pkg/meta/state builds successfully right now, but I'd be surprised
    if much else did; this changes fundamental facts about how things are
    stored. We'll fix more in the morning.

--- pkg/meta/state/validators.go
+++ pkg/meta/state/validators.go
@@ -1,9 +1,9 @@
 package state
 
 import (
+	"encoding/base64"
+
 	"github.com/attic-labs/noms/go/datas"
-	nt "github.com/attic-labs/noms/go/types"
-	util "github.com/oneiro-ndev/noms-util"
 	"github.com/pkg/errors"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto"
@@ -15,33 +15,28 @@ func (state *Metastate) UpdateValidator(db datas.Database, v abci.ValidatorUpdat
 	pk := v.GetPubKey()
 	pkB, err := pk.Marshal()
 	if err != nil {
-		return errors.Wrap(err, "UpdateValidator: marshalling update public key")
+		return errors.Wrap(err, "Metastate.UpdateValidator->v.PubKey.Marshal")
 	}
-	pkS := util.Blob(db, pkB)
+	pkS := base64.StdEncoding.EncodeToString(pkB)
 	if v.Power == 0 {
-		state.Validators = state.Validators.Edit().Remove(pkS).Map()
+		delete(state.Validators, pkS)
 	} else {
-		powerBlob := util.Int(v.Power).NomsValue()
-		state.Validators = state.Validators.Edit().Set(pkS, powerBlob).Map()
+		state.Validators[pkS] = v.GetPower()
 	}
 	return nil
 }
 
 // GetValidators returns a list of validators this app knows of
 func (state *Metastate) GetValidators() (validators []abci.Validator, err error) {
-	state.Validators.Iter(func(key, value nt.Value) (stop bool) {
-		// extract pubkey from noms
-		var pkB []byte
-		pkB, err = util.Unblob(key.(nt.Blob))
+	for pubkeyB64, power := range state.Validators {
+		pkB, err := base64.StdEncoding.DecodeString(pubkeyB64)
 		if err != nil {
-			err = errors.Wrap(err, "GetValidators: unblob public key")
-			return true // stop iteration
+			return nil, errors.Wrap(err, "Metastate.GetValidators->decode pubkey b64")
 		}
 		pk := abci.PubKey{}
 		err = pk.Unmarshal(pkB)
 		if err != nil {
-			err = errors.Wrap(err, "GetValidators: unmarshal public key")
-			return true
+			return nil, errors.Wrap(err, "GetValidators: unmarshal public key")
 		}
 
 		// transform pubkey to address
@@ -53,24 +48,15 @@ func (state *Metastate) GetValidators() (validators []abci.Validator, err error)
 		var tcpk crypto.PubKey
 		tcpk, err = tmconv.PB2TM.PubKey(pk)
 		if err != nil {
-			err = errors.Wrap(err, "GetValidators: convert tm.abci pk into tm.crypto pk")
-			return true
+			return nil, errors.Wrap(err, "GetValidators: convert tm.abci pk into tm.crypto pk")
 		}
 		address := tcpk.Address()
-
-		// extract power from noms
-		power, err := util.IntFrom(value)
-		if err != nil {
-			err = errors.Wrap(err, "GetValidators: IntFromBlob power")
-			return true
-		}
 
 		// finish
 		validators = append(validators, abci.Validator{
 			Address: address,
-			Power:   int64(power),
+			Power:   power,
 		})
-		return
-	})
+	}
 	return
 }
--- pkg/meta/state/vote_stats.go
+++ pkg/meta/state/vote_stats.go
@@ -14,8 +14,6 @@ import (
 const HistorySize = 20
 
 // generate noms marshalers
-//go:generate go run $GOPATH/src/github.com/oneiro-ndev/generator/cmd/nomsify $GOPATH/src/github.com/oneiro-ndev/metanode/pkg/meta/state
-//go:generate find $GOPATH/src/github.com/oneiro-ndev/metanode/pkg/meta/state -name "*noms_gen*.go" -maxdepth 1 -exec goimports -w {} ;
 //nomsify NodeRoundStats RoundStats VoteStats
 
 // NodeRoundStats contains information about the votes of a particular node in a particular round
