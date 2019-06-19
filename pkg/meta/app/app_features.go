90df9dc9378df3ada988e9bef992627ebf098dbb
Eric Schmidt <eric.schmidt@oneiro.io>
2019-06-19 14:09:17 -0700

    Fix comments

--- pkg/meta/app/app_features.go
+++ pkg/meta/app/app_features.go
@@ -8,22 +8,21 @@ type Feature int
 // For example, if we change validation rules for a transaction, we have to remain backward
 // compatible for playback purposes, in case there are transactions already on the blockchain
 // that got there using the outdated validation rules.  Once we upgrade nodes with the new
-// feature logic, we can do so once we're past a given block height.  IsActive() can return true
-// once that happens.
+// feature logic, the new feature activates once we're past a given block height.
 type Features interface {
 	// IsActive returns whether the given feature is currently active.
 	//
 	// Once a feature becomes "active", it can never become "inactive".  We can handle this when
 	// we add more features that override previous features by checking the newest features first.
 	//
-	// For example, say we add a feature in some transaction validation code that rounded a qty:
+	// For example, say we have a feature in some transaction validation code that rounds a qty:
 	//
 	//   qty := math.Round(tx.Qty)
 	//
 	// Then later we decided to round to the nearest tenth instead, we would write:
 	//
 	//   qty := tx.Qty
-	//   if features.IsActive("RoundToTenths") {
+	//   if features.IsActive(RoundToTenths) {
 	//       qty = math.Round(qty*10)/10
 	//   } else {
 	//       qty = math.Round(qty)
@@ -32,9 +31,9 @@ type Features interface {
 	// Then even later we decide to round to the nearest hundredth, we would write:
 	//
 	//   qty := tx.Qty
-	//   if features.IsActive("RoundToHundredths") {
+	//   if features.IsActive(RoundToHundredths) {
 	//       qty = math.Round(qty*100)/100
-	//   } else if features.IsActive("RoundToTenths") {
+	//   } else if features.IsActive(RoundToTenths) {
 	//       qty = math.Round(qty*10)/10
 	//   } else {
 	//       qty = math.Round(qty)
