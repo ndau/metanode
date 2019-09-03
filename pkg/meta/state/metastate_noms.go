88cf7b8e9cf44fd00bc8e66885438184bce068d1
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2019-09-03 15:03:28 +0200

    regenerate generated code, which tracks new noms IterFields signature

--- pkg/meta/state/metastate_noms.go
+++ pkg/meta/state/metastate_noms.go
@@ -15,6 +15,22 @@ import (
 // compilation errors, so that humans can appropriately edit the generated code.
 // search for "WARNING WARNING" to find the appropriate section.
 
+// Adding new fields to a nomsify-able struct:
+//
+// Managed vars are useful for adding new fields that are marshaled to noms only after they're
+// first set, so that app hashes aren't affected until the new fields are actually needed.
+//
+// A managed vars map is a hash map whose keys are managed variable names.
+// The `managedVars map[string]struct{}` field must be manually declared in the struct.
+//
+// Declare new fields using the "managedVar" prefix.  e.g. `managedVarSomething SomeType`.
+// GetSomething() and SetSomething() are generated for public access to the new field.
+//
+// Once SetSomething() is called for the first time, typically as a result of processing a new
+// transaction that uses it, the managed vars map will contain "Something" as a key and the
+// value of managedVarSomething will be stored in noms on the next call to MarshalNoms().
+// Until then, all new managedVar fields will retain their "zero" values.
+
 var metastateStructTemplate nt.StructTemplate
 
 func init() {
@@ -29,14 +45,11 @@ func init() {
 // MarshalNoms implements noms/go/marshal.Marshaler
 func (x Metastate) MarshalNoms(vrw nt.ValueReadWriter) (metastateValue nt.Value, err error) {
 	// x.Validators (map[string]int64->*ast.MapType) is primitive: false
-
 	// template decompose: x.Validators (map[string]int64->*ast.MapType)
 	// template map: x.Validators
 	validatorsKVs := make([]nt.Value, 0, len(x.Validators)*2)
 	for validatorsKey, validatorsValue := range x.Validators {
-
 		// template decompose: validatorsValue (int64->*ast.Ident)
-
 		validatorsKVs = append(
 			validatorsKVs,
 			nt.String(validatorsKey),
@@ -47,7 +60,6 @@ func (x Metastate) MarshalNoms(vrw nt.ValueReadWriter) (metastateValue nt.Value,
 	// x.Height (uint64->*ast.Ident) is primitive: true
 
 	// x.Stats (VoteStats->*ast.Ident) is primitive: false
-
 	// template decompose: x.Stats (VoteStats->*ast.Ident)
 	// template nomsmarshaler: x.Stats
 	statsValue, err := x.Stats.MarshalNoms(vrw)
@@ -56,7 +68,6 @@ func (x Metastate) MarshalNoms(vrw nt.ValueReadWriter) (metastateValue nt.Value,
 	}
 
 	// x.ChildState (State->*ast.Ident) is primitive: false
-
 	// template decompose: x.ChildState (State->*ast.Ident)
 	// template nomsmarshaler: x.ChildState
 	childStateValue, err := x.ChildState.MarshalNoms(vrw)
@@ -64,18 +75,18 @@ func (x Metastate) MarshalNoms(vrw nt.ValueReadWriter) (metastateValue nt.Value,
 		return nil, errors.Wrap(err, "Metastate.MarshalNoms->ChildState.MarshalNoms")
 	}
 
-	return metastateStructTemplate.NewStruct([]nt.Value{
+	values := []nt.Value{
 		// x.ChildState (State)
 		childStateValue,
 		// x.Height (uint64)
-
 		util.Int(x.Height).NomsValue(),
 		// x.Stats (VoteStats)
 		statsValue,
 		// x.Validators (map[string]int64)
-
 		nt.NewMap(vrw, validatorsKVs...),
-	}), nil
+	}
+
+	return metastateStructTemplate.NewStruct(values), nil
 }
 
 var _ marshal.Marshaler = (*Metastate)(nil)
@@ -97,87 +108,86 @@ func (x *Metastate) UnmarshalNoms(value nt.Value) (err error) {
 	// the struct until it finds one whose name happens to match the one sought.
 	// It's better to iterate once over the struct and set the fields of the
 	// target struct in arbitrary order.
-	vs.IterFields(func(name string, value nt.Value) {
-		if err == nil {
-			switch name {
-			// x.Validators (map[string]int64->*ast.MapType) is primitive: false
-			case "Validators":
-				// template u_decompose: x.Validators (map[string]int64->*ast.MapType)
-				// template u_map: x.Validators
-				if validatorsMap, ok := value.(nt.Map); ok {
-					x.Validators = make(map[string]int64)
-					validatorsMap.Iter(func(validatorsKey, validatorsValue nt.Value) (stop bool) {
-						validatorsKeyString, ok := validatorsKey.(nt.String)
-						if !ok {
-							err = fmt.Errorf(
-								"Metastate.UnmarshalNoms expected validatorsKey to be a nt.String; found %s",
-								reflect.TypeOf(validatorsKey),
-							)
-							return true
-						}
+	vs.IterFields(func(name string, value nt.Value) (stop bool) {
+		switch name {
+		// x.Validators (map[string]int64->*ast.MapType) is primitive: false
+		case "Validators":
+			// template u_decompose: x.Validators (map[string]int64->*ast.MapType)
+			// template u_map: x.Validators
+			validatorsGMap := make(map[string]int64)
+			if validatorsNMap, ok := value.(nt.Map); ok {
+				validatorsNMap.Iter(func(validatorsKey, validatorsValue nt.Value) (stop bool) {
+					validatorsKeyString, ok := validatorsKey.(nt.String)
+					if !ok {
+						err = fmt.Errorf(
+							"Metastate.UnmarshalNoms expected validatorsKey to be a nt.String; found %s",
+							reflect.TypeOf(validatorsKey),
+						)
+						return true
+					}
 
-						// template u_decompose: validatorsValue (int64->*ast.Ident)
-						// template u_primitive: validatorsValue
-						var validatorsValueValue util.Int
-						validatorsValueValue, err = util.IntFrom(validatorsValue)
-						if err != nil {
-							err = errors.Wrap(err, "Metastate.UnmarshalNoms->validatorsValue")
-							return
-						}
-						validatorsValueTyped := int64(validatorsValueValue)
-						if err != nil {
-							return true
-						}
-						x.Validators[string(validatorsKeyString)] = validatorsValueTyped
-						return false
-					})
-				} else {
-					err = fmt.Errorf(
-						"Metastate.UnmarshalNoms expected x.Validators to be a nt.Map; found %s",
-						reflect.TypeOf(value),
-					)
-				}
-
-			// x.Height (uint64->*ast.Ident) is primitive: true
-			case "Height":
-				// template u_decompose: x.Height (uint64->*ast.Ident)
-				// template u_primitive: x.Height
-				var heightValue util.Int
-				heightValue, err = util.IntFrom(value)
-				if err != nil {
-					err = errors.Wrap(err, "Metastate.UnmarshalNoms->Height")
-					return
-				}
-				heightTyped := uint64(heightValue)
-
-				x.Height = heightTyped
-
-			// x.Stats (VoteStats->*ast.Ident) is primitive: false
-			case "Stats":
-				// template u_decompose: x.Stats (VoteStats->*ast.Ident)
-				// template u_nomsmarshaler: x.Stats
-				var statsInstance VoteStats
-				err = statsInstance.UnmarshalNoms(value)
-				err = errors.Wrap(err, "Metastate.UnmarshalNoms->Stats")
-
-				x.Stats = statsInstance
-
-			// x.ChildState (State->*ast.Ident) is primitive: false
-			case "ChildState":
-				// template u_decompose: x.ChildState (State->*ast.Ident)
-				// template u_nomsmarshaler: x.ChildState
-
-				// WARNING WARNING WARNING WARNING WARNING WARNING WARNING
-				// this code is hand-edited: we have as a precondition that
-				// x.ChildState is a non-nil instance of the State interface;
-				// this is mandatory to get this unmarshaller to work.
-				//
-				// In general, when not dealing with interfaces, the generated
-				// code will work fine, but we can't rely on that right now.
-				err = x.ChildState.UnmarshalNoms(value)
-				err = errors.Wrap(err, "Metastate.UnmarshalNoms->ChildState")
+					// template u_decompose: validatorsValue (int64->*ast.Ident)
+					// template u_primitive: validatorsValue
+					var validatorsValueValue util.Int
+					validatorsValueValue, err = util.IntFrom(validatorsValue)
+					if err != nil {
+						err = errors.Wrap(err, "Metastate.UnmarshalNoms->validatorsValue")
+						return
+					}
+					validatorsValueTyped := int64(validatorsValueValue)
+					if err != nil {
+						return true
+					}
+					validatorsGMap[string(validatorsKeyString)] = validatorsValueTyped
+					return false
+				})
+			} else {
+				err = fmt.Errorf(
+					"Metastate.UnmarshalNoms expected validatorsGMap to be a nt.Map; found %s",
+					reflect.TypeOf(value),
+				)
 			}
+
+			x.Validators = validatorsGMap
+		// x.Height (uint64->*ast.Ident) is primitive: true
+		case "Height":
+			// template u_decompose: x.Height (uint64->*ast.Ident)
+			// template u_primitive: x.Height
+			var heightValue util.Int
+			heightValue, err = util.IntFrom(value)
+			if err != nil {
+				err = errors.Wrap(err, "Metastate.UnmarshalNoms->Height")
+				return
+			}
+			heightTyped := uint64(heightValue)
+
+			x.Height = heightTyped
+		// x.Stats (VoteStats->*ast.Ident) is primitive: false
+		case "Stats":
+			// template u_decompose: x.Stats (VoteStats->*ast.Ident)
+			// template u_nomsmarshaler: x.Stats
+			var statsInstance VoteStats
+			err = statsInstance.UnmarshalNoms(value)
+			err = errors.Wrap(err, "Metastate.UnmarshalNoms->Stats")
+
+			x.Stats = statsInstance
+		// x.ChildState (State->*ast.Ident) is primitive: false
+		case "ChildState":
+			// template u_decompose: x.ChildState (State->*ast.Ident)
+			// template u_nomsmarshaler: x.ChildState
+
+			// WARNING WARNING WARNING WARNING WARNING WARNING WARNING
+			// this code is hand-edited: we have as a precondition that
+			// x.ChildState is a non-nil instance of the State interface;
+			// this is mandatory to get this unmarshaller to work.
+			//
+			// In general, when not dealing with interfaces, the generated
+			// code will work fine, but we can't rely on that right now.
+			err = x.ChildState.UnmarshalNoms(value)
+			err = errors.Wrap(err, "Metastate.UnmarshalNoms->ChildState")
 		}
+		stop = err != nil
+		return
 	})
 	return
 }
