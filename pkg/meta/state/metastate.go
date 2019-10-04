caf1ea52c1d56296ea6606f4094b1c216e389093
Kent Quirk <kentquirk@gmail.com>
2019-10-04 17:14:55 -0400

    Inject copyright info.

--- pkg/meta/state/metastate.go
+++ pkg/meta/state/metastate.go
@@ -10,6 +10,15 @@ import (
 	"github.com/pkg/errors"
 )
 
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 // Metastate wraps the client app state and keeps track of bookkeeping data
 // such as the validator set and height offset.
 //nomsify Metastate
